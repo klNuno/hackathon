23bf6d9cd53a044e517cf255f892b020f51fff29
klNuno <nefreex@gmail.com>
2022-06-09 22:09:29 +0200

    potentiometre.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <c_la_sauce.h>
+#include "mbed.h"
 
-int main()
+
+AnalogIn ain(PC_0);
+DigitalOut dout(LED1);
+
+int main(void)
 {
-    
+    while (1) {
+        if(ain > 0.3f) {
+            dout = 1;
+        } else {
+            dout = 0;
+        }
+
+        printf("normalized: 0x%04X \n", ain.read_u16());
+        wait_ns(1000000000);
+    }
 }
