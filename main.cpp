8e3815e978376216edfc297b27346e2e4ca2b32a
meetsu <96637888+klNuno@users.noreply.github.com>
2022-06-11 12:03:39 +0200

    hackathon final

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include "mbed.h"
+#include <cstdlib>
 
-int main(void)
-{
+
+DigitalOut stop(PF_2,0);
+DigitalOut direction(PE_3,0);
+DigitalOut direction2(PE_10,1);
+PwmOut sah(PE_6);
+PwmOut sah2(PE_5);
+
+int main(){
+    sah.period_us(50);
+    sah2.period_us(50);
+    sah = 1;
+    sah2 = 1;
+    ThisThread::sleep_for(5s);
+    stop=1;
 }
